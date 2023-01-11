b1a0ca123f27069c494062f59307101d37d8e5db
J03B <byucrazyfan@gmail.com>
2023-01-10 22:10:36 -0600

    Fetched categories from APIs and added as options

--- assets/js/script-landing.js
+++ assets/js/script-landing.js
@@ -3,43 +3,95 @@ var openTdbRootURL = 'https://opentdb.com/api.php';
 var theTriviaRootURL = 'https://the-trivia-api.com/api';
 var scoreHistory = [];
 var categoryList = [];
+    // categoryList is a list of objects with the follow values per line
+    //      catName: ____
+    //      APIused: "theTrivia" or "openTdb"
+    //      id: (Note this is the underscore separated term for theTriviaAPI)
 
 // Ease of Access for DOM elements - references
-var categorySection = document.querySelector('#');
-var categoryInput = document.querySelector('#');
-var difficultySection = document.querySelector('#');
-var difficultyInput = document.querySelector('#');
-var numQsSection = document.querySelector('#');
-var numQsInput = document.querySelector('#');
-var startGameButton = document.querySelector('#');
+var nameInput = document.querySelector('#player-name')
+var categorySection = document.querySelector('#categories-select');
+//var difficultySection = document.querySelector('');
+var difficultyInput = document.querySelector('#difficulty-select');
+//var numQsSection = document.querySelector('');
+var numQsInput = document.querySelector('#number-of-questions');
+var startGameButton = document.querySelector('#start-button');
 
 // Function to alphabatize the categories between the two APIs
 function alphabatizeCategories() {
-
+    categoryList.sort(function (a,b) {
+        if (a.catName < b.catName) {
+            return -1;
+        }
+        if (a.catName > b.catName) {
+            return 1;
+        }
+        return 0;
+    });
 }
 
 // Function to add a game category from the APIs
 function addCategoryOption(category) {
-
+    var newOption = document.createElement('option');
+    newOption.textContent = category.catName;
+    newOption.classList.add(`data-id:${category.id}`,`data-api:${category.APIused}`);
+    categorySection.append(newOption);
 }
 
 function initCategories() {
     // call APIs to get each possible category
-    fetch(`${theTriviaRootURL}/questions`);
-    fetch('https://opentdb.com/api_category.php');
+    fetch(`${theTriviaRootURL}/categories`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (cats) {
+            Object.entries(cats).forEach(([key,value]) => {
+                var newCat = {
+                    catName: key,
+                    APIused: 'theTrivia',
+                    id: value[0]
+                };
+                categoryList.push(newCat);
+                alphabatizeCategories(); // Have to call each time we add a new value to the array
+            });
+        })
+        .catch(function (er) {
+            console.error(er);
+        });
 
-    // Store each category in categoryList
-
-        // Each item in the list should be an object in the following format
-        //  var triviaCat = {
-        //      catName: ____
-        //      APIused: "theTrivia" or "openTdb"
-        //      id: (Note this is the underscore separated term for theTriviaAPI)
-        //  }
+    fetch('https://opentdb.com/api_category.php')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (cats) {
+            Object.entries(cats.trivia_categories).forEach(cat => {
+                var newCat = {
+                    catName: cat[1].name,
+                    APIused: 'openTdb',
+                    id: cat[1].id
+                };
+                categoryList.push(newCat);
+                alphabatizeCategories();
+            });
+        })
+        .catch(function (er) {
+            console.error(er);
+        });
 
     // Call alphabatizeCategories() to order the list by catName
-
+    alphabatizeCategories();
+    
     // Loop through addCategoryOption to add each option to the form
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const addAllCats = async () => {
+        await delay(1000);
+        console.log('waited 1 second to load categories from APIs');
+        for (let i = 0; i < categoryList.length; i++) {
+            const cat = categoryList[i];
+            addCategoryOption(cat);
+        }
+    }
+    addAllCats();
 }
 
 // Function to disable the difficulty setting based on the API chosen
@@ -50,7 +102,12 @@ function toggleDifficulty (event) {
 
 // Function to save form responses to local storage as game settings
 function saveSettings() {
-
+    var settings = {
+        playerName: nameInput.value,
+        category: categorySection.value,
+        numQ: numQsInput.value
+    }
+    localStorage.setItem('no-mercy-settings',JSON.stringify(settings));
 }
 
 // Function to handle the form start based on the selected criteria
@@ -58,8 +115,9 @@ function startGame(event) {
     event.preventDefault();
     saveSettings();
     // open up play.html
+    window.location.href = 'play.html';
 }
 
 initCategories();
-categorySection.addEventListener('submit',toggleDifficulty);
+//categorySection.addEventListener('submit',toggleDifficulty);
 startGameButton.addEventListener('click',startGame);
